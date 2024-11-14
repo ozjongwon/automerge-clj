6a18a2d640ea2d9a8acc2175fbed84dad1d6d04c
jc <oz.jongwon.choi@gmail.com>
2024-11-14 17:15:45 +1100

    cleanup #1

--- src/clojure/automerge_clj/generate_interface.clj
+++ src/clojure/automerge_clj/generate_interface.clj
@@ -6,239 +6,8 @@
    [clojure.string :as str]
    [clojure.set :as set]
    [camel-snake-kebab.core :as csk])
-  (:import [java.util Optional List HashMap Date Iterator ArrayList]
-           [org.automerge ObjectId ObjectType ExpandMark Conflicts
-            Document Transaction ChangeHash Cursor PatchLog SyncState NewValue
-            AmValue ObjectId Patch PatchAction Mark Prop Prop$Key Prop$Index
-            PatchAction$PutMap PatchAction$PutList PatchAction$Increment PatchAction$FlagConflict
-            PatchAction$Mark
-            PatchAction$PutList PatchAction$Insert PatchAction$SpliceText PatchAction$DeleteList
-            AmValue$Map AmValue$List AmValue$Text AmValue$UInt AmValue$Int AmValue$Bool AmValue$Bytes
-            AmValue$Str AmValue$F64 AmValue$Counter AmValue$Timestamp AmValue$Null AmValue$Unknown
-            Counter]))
-
-(defonce ^:dynamic *fout* nil)
-
-(defn- simple-case? [defs]
-  (let [num-arg-list (map #(count (nth % 4)) defs)]
-    (or (empty? num-arg-list)
-        (= (count num-arg-list)
-           (count (dedupe num-arg-list))))))
-
-(defn- split-type+args [type+args]
-  (loop [[[type arg] & more-defs] type+args types [] args []]
-    (cond (nil? type) [types args]
-          :else
-          (recur more-defs (conj types type) (conj args arg)))))
-
-(defn- canonical-type [type]
-  (cond (nil? type) nil
-
-        (= 'int type) "^Integer"
-        (= 'double type) "^Double"
-        (= 'long type) "^Long"
-
-        (= 'boolean type) "^Boolean"
-
-        (symbol? type) (str "^" type)
-
-        (= '[:array-of byte] type) "^bytes"
-
-        (= (first type) :array-of)
-        (format "^\"%s\"" (.getName (class (make-array (resolve (second type)) 0))))
-
-        :else (throw (ex-info "Unknown type" {:type type}))))
-
-(defn- make-main-instance-arg [java-class & {:keys [static? constructor?]}]
-  (when-not (or static? constructor?)
-    [(canonical-type java-class) (symbol (str/lower-case java-class))]))
-
-(defn- maybe-call-converter [args]
-  (vec (mapcat (fn [[tp arg]]
-                 (case tp
-                   "^Long" `[~tp (~'long ~arg)]
-                   "^Integer" `[~tp (~'int ~arg)]
-                   "^Double" `[~tp (~'double ~arg)]
-                   "^Boolean" `[~tp (~'boolean ~arg)]
-                   [tp arg]))
-               (partition 2 args))))
-
-(defn- make-java-call-signature [java-class java-fn args & {:keys [static? constructor?]}]
-  (let [new-args (maybe-call-converter args)]
-    (cond constructor? `(~(str java-class \.) ~@new-args)
-          static? `(~(str java-class "/" java-fn) ~@new-args)
-          :else `(~(str \. java-fn) ~(str/lower-case java-class) ~@new-args))))
-
-(defn- java->clj-fname+main-arg [tclass class method]
-  (let [clj-fn (csk/->kebab-case method)]
-    (cond (= tclass class method) [(str "make-" clj-fn) tclass] ;; ctor
-          (= tclass class) [clj-fn tclass]
-          :else [clj-fn class])))
-
-(defn- generate-a-function [[tclass class java-fn return-type type+args &
-                             {:keys [static? constructor?] :as opts}]]
-  (let [[fn-name java-class] (java->clj-fname+main-arg tclass class java-fn)
-        [types args] (split-type+args type+args)
-        canonicalized-args (mapcat (fn [type arg]
-                                     (list (canonical-type type) arg))
-                                   types args)
-        fn-args (take-nth 2 (rest canonicalized-args))]
-    (pp/cl-format *fout* "~2&(defn ~{~A~} ~A [~{~A~^ ~}]~%~2T(~{~A~^ ~}))~&"
-                  (when-let [result (canonical-type return-type)]
-                    result)
-                  fn-name
-                  `(~@(make-main-instance-arg java-class opts)
-                    ~@fn-args)
-                  (make-java-call-signature java-class java-fn canonicalized-args opts))))
-
-(defn- generate-simple-multiple-arity-functions [defs]
-  (assert (apply = (map #(nth % 3) defs)) "Return type must be same")
-  (let [[tclass class java-fn return-type type+args & {:keys [static? constructor?] :as opts}] (first defs)
-        [fn-name java-class] (java->clj-fname+main-arg tclass class java-fn)]
-    (pp/cl-format *fout* "~2&(defn ~{~A~} ~A~%~{~A~^~%~})~&"
-                  (when-let [result (canonical-type return-type)]
-                    result)
-                  fn-name
-                  (run! (fn [[_ _ _ _ type+args]]
-                          (let [[types args] (split-type+args type+args)
-                                canonicalized-args (mapcat (fn [type arg]
-                                                             (list (canonical-type type) arg))
-                                                           types args)
-                                fn-args (take-nth 2 (rest canonicalized-args))]
-                            (pp/cl-format nil "~2T([~{~A~^ ~}]~%~2T (~{~A~^ ~}))"
-                                          `(~@(make-main-instance-arg java-class opts)
-                                            ~@fn-args)
-                                          (make-java-call-signature java-class java-fn canonicalized-args opts))))
-                        defs))))
-;;;
-;;; Multiple arity case
-;;;
-(defn- group-by-arity [methods]
-  "Group methods by number of arguments"
-  (group-by #(count (nth % 4)) methods))
-
-(defn clj-type-predicate [type-spec]
-  (case type-spec
-    int 'integer?
-    long 'integer?
-    double 'double?
-    boolean 'boolean?
-    false))
-
-(defn- type-check-expr [arg-num type-spec]
-  (let [arg (symbol (str "arg" arg-num))]
-    (cond (symbol? type-spec)
-          (if-let [predicate (clj-type-predicate type-spec)]
-            `(~predicate ~arg)
-            `(~'instance? ~type-spec ~arg))
-
-          (and (vector? type-spec) (= :array-of (first type-spec)))
-          (let [type (second type-spec)]
-            (cond (= type 'byte) `(~'bytes? ~arg)
-                  :else
-                  `(~'array-instance? ~(second type-spec) ~arg)))
-
-          :else (throw (ex-info "Uknown type-spec" {:type-spec type-spec})))))
-
-(defn- make-cond-clause [java-class java-fn obj [tclass class fname mreturn-type args & opts]]
-  (let [conditions (map-indexed
-                    (fn [idx [type _]]
-                      (type-check-expr (inc idx) type))
-                    args)
-        arg-names (map-indexed
-                   (fn [idx [type name]]
-                     [type (symbol (str "arg" (inc idx)))])
-                   args)
-        call-signature (make-java-call-signature java-class
-                                                 java-fn
-                                                 `(~@(mapcat (fn [[type arg]] [(canonical-type type) arg]) arg-names))
-                                                 opts)]
-    (case (count conditions)
-      0 call-signature
-      1 `(~@conditions ~call-signature)
-      `((~'and ~@conditions)
-        ~(make-java-call-signature java-class
-                                   java-fn
-                                   `(~@(mapcat (fn [[type arg]] [(canonical-type type) arg]) arg-names))
-                                   opts)))))
-
-(defn- make-arity-method [java-fn java-class methods arity opts]
-  (let [arg-symbols (map #(symbol (str "arg" (inc %))) (range arity))
-        [main-obj-type main-obj] (make-main-instance-arg java-class opts)
-        cond-clauses (mapcat #(make-cond-clause java-class java-fn main-obj %) methods)]
-    (if (zero? arity)
-      (pp/cl-format nil "[~{~A~^ ~}]~%~3T~A"
-                    `(~main-obj-type ~main-obj ~@arg-symbols) cond-clauses)
-      (pp/cl-format nil "[~{~A~^ ~}]~%~2T(cond ~{~A~^~%~8T~}~%~8T:else (throw (ex-info \"Type error\" ~S)))"
-                    `(~main-obj-type ~main-obj ~@arg-symbols) cond-clauses (zipmap (map #(keyword (str "arg" (inc %)))
-                                                                                        (range arity))
-                                                                                   arg-symbols)))))
-
-(defn- generate-complex-multiple-arity-functions [defs]
-  (let [[tclass class java-fn return-type _ & opts] (first defs)
-        [fn-name java-class] (java->clj-fname+main-arg tclass class java-fn)
-        grouped (group-by-arity defs)
-        arity-methods (map (fn [[arity methods]]
-                             (make-arity-method java-fn java-class methods arity opts))
-                           grouped)]
-    (pp/cl-format *fout* "~2&(defn ~{~A~} ~A~%~{~2T(~A)~^~%~})~&"
-                  (when-let [result (canonical-type return-type)]
-                    result)
-                  fn-name
-                  arity-methods)))
-
-(defn- generate-multy-arity-functions [defs]
-  (cond (= (count defs) (->> defs (map first) dedupe count))
-        (run! #(generate-a-function %) defs)
-
-        (simple-case? defs) (generate-simple-multiple-arity-functions defs)
-
-        :else (generate-complex-multiple-arity-functions defs)))
-
-(def +unusable-classes+ #{'Counter})
-
-(defn remove-unusable-methods [defs]
-  (filter (fn [[_ _ _ _ params & _]]
-            (-> (map first params)
-                (set)
-                (set/intersection  +unusable-classes+)
-                (empty?)))
-          defs))
-
-(defn generate-functions [clj-defs]
-  (let [filtered-defs (remove-unusable-methods clj-defs)]
-    (if (= (count clj-defs) 1)
-      (generate-a-function (first filtered-defs))
-      (generate-multy-arity-functions filtered-defs))))
-
-(defn generate-clojure-interface [filename]
-  "Reads the file, processes each line, and returns a map of function names to their Clojure definitions."
-  (with-open [reader (clojure.java.io/reader filename)]  ; Open the file for reading
-    (let [reader (java.io.PushbackReader. reader)
-          [k arrow tclass] (list  (read reader false :eof) (read reader false :eof) (read reader false :eof))]
-
-      (assert (and (= :java-name k) (= '=> arrow) (symbol? tclass)) "File format must be valid")
-
-      (->> (loop [result {}]
-             (let [class (read reader false :eof)
-                   arrow (read reader false :eof)
-                   class-fn-return-args (read reader false :eof)]
-               (when-not (= :eof class)
-                 (assert (and (symbol? class) (= '=> arrow) (list? class-fn-return-args))
-                         "Entry format must be valid"))
-               (if (= :eof class)
-                 result
-                 (recur (update result
-                                (second class-fn-return-args) ;; fn is the key
-                                (fnil conj [])
-                                (cons tclass class-fn-return-args))))))
-           (map (fn [[fname tclass-class-fn-return-args]]
-                  (generate-functions tclass-class-fn-return-args)))))))
-
-;; (defn long? [x]
-;;   (and (integer? x)
-;;        (or (> x Integer/MAX_VALUE)
-;;            (< x Integer/MIN_VALUE))))
+  (:import [org.automerge ChangeHash ;; To generate type hint for the array of ChangeHash
+            ]))
 
 
 (defonce special-case-functions 
@@ -264,39 +33,6 @@
   (and (-> o class .isArray)
        (= (-> o class .getComponentType)
           c)))")
-
-(defn- interface-header [classes-to-import]
-  (binding [*out* *fout*]
-    (println ";;;\n;;; Generated file, do not edit\n;;;\n")
-    (pp/cl-format *fout* "(ns clojure.automerge-clj.automerge-interface
-        (:import [java.util Optional List HashMap Date Iterator ArrayList]
-                 [org.automerge ObjectId ObjectType ExpandMark~%~{~A~^ ~}]))~2&~A~2&"
-                  classes-to-import
-                  special-case-functions)))
-
-(defn generate-clojure-interface [filename]
-  "Reads the file, processes each line, and returns a map of function names to their Clojure definitions."
-  (with-open [reader (clojure.java.io/reader filename)]  ; Open the file for reading
-    (let [reader (java.io.PushbackReader. reader)
-          [k arrow tclass] (list  (read reader false :eof) (read reader false :eof) (read reader false :eof))]
-
-      (assert (and (= :java-name k) (= '=> arrow) (symbol? tclass)) "File format must be valid")
-
-      (->> (loop [result {}]
-             (let [class (read reader false :eof)
-                   arrow (read reader false :eof)
-                   class-fn-return-args (read reader false :eof)]
-               (when-not (= :eof class)
-                 (assert (and (symbol? class) (= '=> arrow) (list? class-fn-return-args))
-                         "Entry format must be valid"))
-               (if (= :eof class)
-                 result
-                 (recur (update result
-                                (second class-fn-return-args) ;; fn is the key
-                                (fnil conj [])
-                                (cons tclass class-fn-return-args))))))
-           (map (fn [[fname tclass-class-fn-return-args]]
-                  (generate-functions tclass-class-fn-return-args)))))))
 
 ;; clj-fn-entry method tclass class clj-fn return args opts
 (defn- clj-fn-entry [method tclass class clj-fn return args {:keys [static? constructor?]}]
@@ -347,28 +83,7 @@
       result)))
 
 (defn- group-by-n-args [methods]
-  ;;{:keys [return fn fn-args method method-args]}
   (group-by #(count (:fn-args %)) methods))
-
-;; (cond (nil? type) "void"
-;;       (symbol? type) (str "" type)
-
-;;       (= '[:array-of int] type)  "ints"
-;;       (= '[:array-of long] type) "longs"
-;;       (= '[:array-of float] type) "floats"
-;;       (= '[:array-of double] type) "dboubles"
-;;       (= '[:array-of short] type) "shorts"
-;;       (= '[:array-of boolean] type) "booleans"
-;;       (= '[:array-of byte] type) "bytes"        
-;;       (= '[:array-of char] type) "chars"
-;;       (= '[:array-of object] type) "objects"
-
-;;       (= (first type) :array-of)
-;;       (format "\"%s\"" (.getName (class (make-array (resolve (second type)) 0))))
-
-;;       :else (throw (ex-info "Unknown type" {:type type})))
-
-
 
 (defn- args->type-check-exps [args]
   (map (fn [[type arg]]
@@ -435,7 +150,10 @@
         (= '[:array-of object] type) "objects"
 
         (= (first type) :array-of)
-        (format "\"%s\"" (.getName (class (make-array (resolve (second type)) 0))))
+        (try 
+          (format "\"%s\"" (.getName (class (make-array (resolve (second type)) 0))))
+          (catch Exception e
+            (println ">>>" type)))
 
         :else (throw (ex-info "Unknown type" {:type type}))))
 
@@ -473,6 +191,24 @@
 (defn def-str-list->file [file str-list]
   (with-open [o (io/writer file)]
     (binding [*out* o]
+      (println ";;;\n;;; Generated file, do not edit\n;;;\n")
+      ;; (pp/cl-format *out* "(ns clojure.automerge-clj.automerge-interface
+
+      ;; (:import [java.util Optional List HashMap Date Iterator ArrayList]
+      ;;      [org.automerge ObjectId ObjectType ExpandMark Conflicts
+      ;;       Document Transaction ChangeHash Cursor PatchLog SyncState NewValue
+      ;;       AmValue ObjectId Patch PatchAction Mark Prop Prop$Key Prop$Index
+      ;;       PatchAction$PutMap PatchAction$Increment PatchAction$FlagConflict
+      ;;       PatchAction$Mark PatchAction$PutList PatchAction$Insert PatchAction$SpliceText
+      ;;       PatchAction$DeleteList AmValue$Map AmValue$List AmValue$Text AmValue$UInt
+      ;;       AmValue$Int AmValue$Bool AmValue$Bytes AmValue$Str AmValue$F64 AmValue$Counter
+      ;;       AmValue$Timestamp AmValue$Null AmValue$Unknown Counter])
+      ;;   (:import [java.util Optional List HashMap Date Iterator ArrayList]
+      ;;            [org.automerge ObjectId ObjectType ExpandMark~%~{~A~^ ~}]))~2&~A~2&"
+      ;;               ;; FIXME
+      ;;               ;;                    classes-to-import
+      ;;               ;;                    special-case-functions
+      ;;               )
       (run! #(apply println %) str-list))))
 
 (defn java->clojure-interface-file [java-files clj-file]
